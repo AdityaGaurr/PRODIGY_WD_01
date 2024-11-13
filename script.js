b36ce7e8d4548c9b5e7b2308c8c82a571899d097
AdityaGaurr <gauraditya63@gmail.com>
2024-11-13 21:59:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Add scroll event listener to change navbar background color on scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
